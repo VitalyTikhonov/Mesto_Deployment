c9a7e0d0e998240a46aeef1aaaee3a946f89b9fb
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-09-10 13:35:50 +0400

    S15 C12 Unpromisified isUserExistent in Cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,23 +18,44 @@ function getAllCards(req, res, next) {
     .catch(next);
 }
 
-function createCard(req, res, next) {
+async function createCard(req, res, next) {
   try {
     const owner = req.user._id; // не менять owner на user!
     isObjectIdValid(owner, 'user');
     const { name, link } = req.body;
-    isUserExistent(owner)
-      .then((checkResult) => {
-        if (checkResult) {
-          return Card.create({ name, link, owner })
-            .then((respObj) => res.send(respObj))
-            .catch((err) => {
-              if (err instanceof mongoose.Error.ValidationError) {
-                next(new InvalidInputError(err));
-              }
-            });
+    if (!await isUserExistent(owner)) {
+      throw new InvalidIdentityError();
+    }
+    Card.create({ name, link, owner })
+      .then((respObj) => res.send(respObj))
+      .catch((err) => {
+        if (err instanceof mongoose.Error.ValidationError) {
+          next(new InvalidInputError(err));
         }
-        throw new InvalidIdentityError();
+      });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteCard(req, res, next) {
+  try {
+    const userId = req.user._id;
+    isObjectIdValid(userId, 'user');
+    const { cardId } = req.params;
+    isObjectIdValid(cardId, 'card');
+    if (!await isUserExistent(userId)) {
+      throw new InvalidIdentityError();
+    }
+    Card.findById(cardId)
+      .orFail(new DocNotFoundError('card'))
+      .then((respObj) => {
+        if (respObj.owner.equals(userId)) {
+          respObj.deleteOne()
+            .then((deletedObj) => res.send(deletedObj));
+        } else {
+          next(new NoRightsError());
+        }
       })
       .catch(next);
   } catch (err) {
@@ -42,75 +63,32 @@ function createCard(req, res, next) {
   }
 }
 
-function deleteCard(req, res, next) {
+async function toggleCardLike(req, res, next) {
   try {
     const userId = req.user._id;
     isObjectIdValid(userId, 'user');
     const { cardId } = req.params;
     isObjectIdValid(cardId, 'card');
-    isUserExistent(userId)
-      .then((checkResult) => {
-        if (checkResult) {
-          return Card.findById(cardId)
-            .orFail(new DocNotFoundError('card'))
-            .then((respObj) => {
-              if (respObj.owner.equals(userId)) {
-                respObj.deleteOne()
-                  .then((deletedObj) => res.send(deletedObj));
-              } else {
-                next(new NoRightsError());
-              }
-            })
-            .catch(next);
-        }
-        throw new InvalidIdentityError();
-      })
-      .catch(next);
-  } catch (err) {
-    next(err);
-  }
-}
-
-function toggleCardLike(req, res, next) {
-  try {
-    const userId = req.user._id;
-    isObjectIdValid(userId, 'user');
-    const { cardId } = req.params;
-    isObjectIdValid(cardId, 'card');
-    isUserExistent(userId)
-      .then((checkResult) => {
-        if (checkResult) {
-          // let action = req.method === 'PUT' ? '$addToSet' : '$pull';
-          // action = req.method === 'DELETE' ? '$pull' : '$addToSet';
-
-          // let action;
-          // if (req.method === 'PUT') {
-          //   action = '$addToSet';
-          // } else if (req.method === 'DELETE') {
-          //   action = '$pull';
-          // }
-
-          let action;
-          switch (req.method) {
-            case 'PUT':
-              action = '$addToSet';
-              break;
-            case 'DELETE':
-              action = '$pull';
-              break;
-            default:
-          }
-          return Card.findByIdAndUpdate(
-            cardId,
-            { [action]: { likes: userId } },
-            { new: true },
-          )
-            .orFail(new DocNotFoundError('card'))
-            .then((respObj) => res.send(respObj))
-            .catch(next);
-        }
-        throw new InvalidIdentityError();
-      })
+    if (!await isUserExistent(userId)) {
+      throw new InvalidIdentityError();
+    }
+    let action;
+    switch (req.method) {
+      case 'PUT':
+        action = '$addToSet';
+        break;
+      case 'DELETE':
+        action = '$pull';
+        break;
+      default:
+    }
+    Card.findByIdAndUpdate(
+      cardId,
+      { [action]: { likes: userId } },
+      { new: true },
+    )
+      .orFail(new DocNotFoundError('card'))
+      .then((respObj) => res.send(respObj))
       .catch(next);
   } catch (err) {
     next(err);
